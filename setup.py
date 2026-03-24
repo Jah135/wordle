e49b7c66e71b0820316eb76d7cbbd679ff48b3b7
Elijah <ehopper848@phm.k12.in.us>
2026-03-24 11:12:58 -0400

    fix setup

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='wordle',
-	version='0.1.0',
-	description='',
-	url='https://github.com/Jah135/wordle',
-	author='Elijah Hopper',
-	license='MIT',
-	packages=find_packages(),
-	python_requires='>=3.12',
+    name="wordle",
+    version="0.1.0",
+    description="",
+    url="https://github.com/Jah135/wordle",
+    author="Elijah Hopper",
+    license="MIT",
+    packages=find_packages(),
+    package_data={"": ["*.txt"]},
+    include_package_data=True,
+    python_requires=">=3.12",
 )
