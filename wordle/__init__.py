be5940fe0e29a81a47535db96d9cfceaa7553462
Elijah <ehopper848@phm.k12.in.us>
2026-03-24 11:21:28 -0400

    remove dictionaries

--- wordle/__init__.py
+++ wordle/__init__.py
@@ -1,6 +1,3 @@
 from .game import WordleGame, LetterValidity
 from .player import WordlePlayer
-
-
-from .dictionary import WORD_DICTIONARY, PLAYABLE_DICTIONARY
 from .local import LocalWordleGame, LocalWordlePlayer
--- wordle/dictionary/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib.resources import files
-
-root = files("wordle.dictionary")
-
-with root.joinpath("La.txt").open("r") as f:
-    WORD_DICTIONARY = [x.strip() for x in f.readlines()]
-with root.joinpath("Ta.txt").open("r") as f:
-    PLAYABLE_DICTIONARY = [*WORD_DICTIONARY, *(x.strip() for x in f.readlines())]
