4b704879aed7b11f88728f3c52f870f898813f57
Elijah <ehopper848@phm.k12.in.us>
2026-03-25 15:28:07 +0000

    update WordleGame interface

--- wordle/game.py
+++ wordle/game.py
@@ -1,10 +1,26 @@
 from enum import IntEnum
 
+
 class LetterValidity(IntEnum):
     Incorrect = 0
     TooMany = 1
     Exists = 2
     Correct = 4
 
+
+Guess = tuple[str, list[LetterValidity]]
+
+
 class WordleGame:
-    def make_guess(self, word: str) -> tuple[str, list[LetterValidity]]: ...
+    is_done: bool = False
+    is_won: bool = False
+    guess_history: list[Guess]
+
+    def __init__(self) -> None:
+        self.guess_history = []
+
+    def check_validity(self, word: str) -> list[LetterValidity]: ...
+    def make_guess(self, word: str) -> Guess:
+        guess = (word, self.check_validity(word=word))
+        self.guess_history.append(guess)
+        return guess
