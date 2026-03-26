f07001ec59fbc74ee8151cb9e1cf3070584b0b72
Elijah <ehopper848@phm.k12.in.us>
2026-03-26 18:15:16 +0000

    g

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wordle",
-    version="0.2.4",
+    version="0.2.5",
     description="",
     url="https://github.com/Jah135/wordle",
     author="Elijah Hopper",
